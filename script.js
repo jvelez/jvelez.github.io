3c0162fa6e5412f4f6f1ca940e3e13a00b1be0be
Jonathan <jvelezal@gmail.com>
2017-11-12 14:17:13 -0400

    portfolio
    portfolio website

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function()
+{
+	/*$(".navb-item").click(function(){
+       $(".navb-item").removeClass("current");
+       $(this).addClass("current");
+    });*/
+
+	//SET CAROUSEL
+	$('.carousel.carousel-slider').carousel({full_width: true});
+	window.setInterval(function(){$('.carousel').carousel('next')},2000);
+
+	//ANIMATE NAVBAR CSS
+	$(window).scroll(function (event)
+	{
+    	var scroll = $(window).scrollTop();
+		if(scroll>32)
+		{
+			$('.navbar').css('background-color', 'rgba(0,0,0,0.7)');
+			$('.navbar').css('box-shadow', '0px 0px 5px rgba(0,0,0,0.6)');
+			$('.navbar-item').css('color', 'white');
+		}
+		else
+		{
+			$('.navbar').css('background-color', 'transparent');
+			$('.navbar').css('box-shadow', '0px 0px 0px transparent');
+			$('.navbar-item').css('color', 'rgb(0,72,128)');
+		}
+	});
+
+	//SCROLL TO SEGMENTS
+	$(".navbar-item, .bouncer").click(function()
+	{
+		var id = "#segment_"+$(this).attr('id');
+		$('html, body').animate({ scrollTop: $(id).offset().top + 'px'}, '0.5s');
+	});
+});
